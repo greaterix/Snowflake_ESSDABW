4e7632d92deed1fba8d453255aeabd30adf41ba6
greaterix <manderton@moonlightbusinessgifts.co.uk>
2022-10-02 11:54:55 +0100

    Update streamlit_app.py
    req

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My PArents New Healthy Diner')
 
@@ -19,3 +20,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruites_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruites_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
